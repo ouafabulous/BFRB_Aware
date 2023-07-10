f4f6100ee8dae097ab58309684c343c8bef52a45
Valentin Dewolf <valentin.dewolf@doctolib.com>
2023-07-10 20:08:24 +0200

    update Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,22 @@
-import React, { useState } from 'react'
-import detectCrisis from '../detectCrisis'
+import React, { useState, useRef } from 'react'
 import { Text, View, Button, StyleSheet } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
+import detectCrisis from '../detectCrisis'
 
 const Home = () => {
   const [isCrisis, setIsCrisis] = useState(false)
+  const [isCheckingForCrisises, setIsCheckingForCrisises] = useState(false)
+  const intervalRef = useRef(null)
+
+  const toggleCrisisCheck = () => {
+    if (intervalRef.current == null) {
+      intervalRef.current = setInterval(() => setIsCrisis(detectCrisis()), 1000)
+      setIsCheckingForCrisises(true)
+    } else {
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+      setIsCheckingForCrisises(false)
+    }
+  }
 
   const styles = StyleSheet.create({
     container: {
@@ -18,10 +30,12 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text>BFRB AWARE</Text>
-      <Text>{isCrisis ? 'Crisis in progres' : 'No Crisis'}</Text>
+      <Text>{isCrisis ? 'Crisis in progress' : 'No Crisis'}</Text>
 
-      <Button title='send Notification' onPress={() => setInterval(() => setIsCrisis(detectCrisis()), 3000)} />
-      <StatusBar style='auto' />
+      <Button
+        title={isCheckingForCrisises ? 'Checking For a crisis...' : 'Press to check for a crisis'}
+        onPress={toggleCrisisCheck}
+      />
     </View>
   )
 }
