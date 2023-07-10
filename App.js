5310db2b9a023071c59ed9c6ea19e36a1d205a2a
y <rayane.boucheffa@doctolib.com>
2023-07-10 13:52:45 +0200

    Display notif

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import notifee from '@notifee/react-native'
+import { AndroidColor } from '@notifee/react-native'
 
 export default function App() {
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    })
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>BFRB AWARE</Text>
 
-      <Button title="send Notification" />
-      <StatusBar style="auto" />
+      <Button title='send Notification' onPress={() => onDisplayNotification()} />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
