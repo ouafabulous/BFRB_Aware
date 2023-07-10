1a2e271888c862915ad0af46428e06f695d8d0e6
Valentin Dewolf <valentin.dewolf@doctolib.com>
2023-07-10 17:29:22 +0200

    remove notification and add detectCrisis

--- App.js
+++ App.js
@@ -1,50 +1,30 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
-import notifee from '@notifee/react-native'
-import { AndroidColor } from '@notifee/react-native'
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import detectCrisis from "./detectCrisis";
 
 export default function App() {
-  async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission()
-
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'default',
-      name: 'Default Channel',
-    })
-
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
-        },
-      },
-    })
-  }
+  const [isCrisis, setIsCrisis] = useState(false);
 
   return (
     <View style={styles.container}>
       <Text>BFRB AWARE</Text>
+      <Text>{isCrisis ? "Crisis in progres" : "No Crisis"}</Text>
 
-      <Button title='send Notification' onPress={() => onDisplayNotification()} />
-      <StatusBar style='auto' />
+      <Button
+        title="send Notification"
+        onPress={() => setInterval(() => setIsCrisis(detectCrisis()), 3000)}
+      />
+      <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+});
---
+++ detectCrisis.js
@@ -0,0 +1,4 @@
+export default detectCrisis = () => {
+  return true; // crisis
+  return false; // No crisis
+};
