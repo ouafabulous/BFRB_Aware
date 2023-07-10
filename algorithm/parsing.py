6bdd472c502a8a7d5d938e7f49ce4f3e42db1619
Ariane BONNEL <abonnel@Ariane-Bonnel.local>
2023-07-10 12:19:00 +0200

    Parsing : added heartrate + output_json files

--- algorithm/parsing.py
+++ algorithm/parsing.py
@@ -1,9 +1,17 @@
 import json
+import sys
 
-input_file_path = "raw_data.log"
-output_file_path = "parsed_raw_data.json"
+# Check if the file name is provided as an argument
+if len(sys.argv) < 2:
+    print("Please provide the file name to parse as an argument.")
+    sys.exit(1)
 
-parsed_data = []
+input_file_path = sys.argv[1]
+output_heart_rate = f"./json_outputs/heartRate_{input_file_path.split('.')[0]}.json"
+output_position = f"./json_outputs/position_{input_file_path.split('.')[0]}.json"
+
+parsed_position = []
+parsed_heart_rate = []
 with open(input_file_path, 'r') as file:
     for line in file:
         if '[SENSORS_SYNC][sample]' in line:
@@ -11,9 +19,16 @@ with open(input_file_path, 'r') as file:
             x = int(values[1][2:])
             y = int(values[2][2:])
             z = int(values[3][2:])
-            parsed_data.append({'x': x, 'y': y, 'z': z})
+            parsed_position.append({'x': x, 'y': y, 'z': z})
+        if '[HR_MEASURE][CONTINUOUS]' in line:
+            values = line.split()
+            hr = int(values[2].split('b')[0])
+            parsed_heart_rate.append({'hr': hr})
 
-with open(output_file_path, 'w') as output_file:
-    json.dump(parsed_data, output_file)
+with open(output_position, 'w') as output_file:
+    json.dump(parsed_position, output_file)
+
+with open(output_heart_rate, 'w') as output_file:
+    json.dump(parsed_heart_rate, output_file)
 
 print("Data successfully parsed and saved in JSON format.")
