8669b83aa44e5c194e956ff221797a5525a8bd9e
y <rayane.boucheffa@doctolib.com>
2023-07-10 18:37:54 +0200

    Icons

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar'
 import { Text, View, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import Contacts from './components/Contacts'
 import Guidelines from './components/Guidelines'
 import Home from './components/Home'
@@ -12,7 +13,27 @@ const Tab = createBottomTabNavigator()
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-play-circle' : 'ios-play-circle-outline'
+            } else if (route.name === 'Contacts') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline'
+            } else if (route.name === 'Guidelines') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline'
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name='Guidelines' component={Guidelines} />
         <Tab.Screen name='Home' component={Home} />
         <Tab.Screen name='Contacts' component={Contacts} />
       </Tab.Navigator>
