6269852cff78f82ff68d35767e4541d8e404e266
Pierre-Louis DE TAEYE <pierrelouis.detaeye@withings.com>
2023-07-11 09:56:09 +0200

    [rasp] added timestamp

--- rasp_realtime_uart/real_time_fetching.py
+++ rasp_realtime_uart/real_time_fetching.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import time
+from datetime import datetime
 from windy.tools.device.dut import DUT
 
 
@@ -11,7 +12,12 @@ def save_to_file(data):
     filename = 'log/{}.log.wip'.format(utc)
 
     with open(filename, 'w') as f:
-        f.write(data)
+        read_utc = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.')
+        line_data = data.split('\r\n')
+        timestamp_width = int(1000 / len(line_data))
+        for i, line in enumerate(line_data):
+            timestamp = read_utc + '{:0>3}'.format(timestamp_width * i)
+            f.write('[{}] '.format(timestamp) + line + '\r\n')
     new_file, ext = os.path.splitext(filename)
     os.renames(filename, new_file)
 
