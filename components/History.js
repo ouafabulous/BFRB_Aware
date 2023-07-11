0520165ad78d5df2c35c5f1ad84127f40e2c0a42
Valentin Dewolf <valentin.dewolf@doctolib.com>
2023-07-11 12:15:34 +0200

    update history style

--- components/History.js
+++ components/History.js
@@ -1,21 +1,9 @@
 import * as React from 'react'
 import { useState } from 'react'
-import { Text, Button, View } from 'react-native'
+import { Text, Button, View, StyleSheet, Pressable } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import HistoryChart from './HistoryChart'
 import { setCrisises, getCrisises } from '../lib/crisisStorage'
-
-// const CrisisList = ({ crisises }) => {
-//   if (!crisises) return <Text>Loading...</Text>
-
-//   return (
-//     <View>
-//       {crisises.map((crisis, index) => (
-//         <Text key={index}>{crisis}</Text>
-//       ))}
-//     </View>
-//   )
-// }
 
 const History = () => {
   const [history, setHistory] = useState(null)
@@ -40,21 +28,45 @@ const History = () => {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <HistoryChart history={history} />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          height: 100,
-          padding: 20,
-        }}
-      >
-        <Button title="Add Crisis" onPress={addCrisis} />
-        <Button title="Edit History" onPress={clearCrisises} />
+      <View style={styles.linkContainer}>
+        <Pressable onPress={addCrisis}>
+          <Text style={styles.link}>Add Crisis</Text>
+        </Pressable>
+        <Pressable onPress={clearCrisises}>
+          <Text style={styles.link}>Edit History</Text>
+        </Pressable>
       </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontFamily: 'Cochin',
+    marginBottom: 64,
+  },
+  link: {
+    color: '#2e7eff',
+  },
+  linkContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 100,
+    padding: 20,
+  },
+})
 
 export default History
