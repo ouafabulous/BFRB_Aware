9962b1d0f70be2d01bf974c10918315c913825b1
Valentin Dewolf <valentin.dewolf@doctolib.com>
2023-07-11 11:12:37 +0200

    add HistoryChart

--- components/History.js
+++ components/History.js
@@ -2,6 +2,37 @@ import * as React from 'react'
 import { useState } from 'react'
 import { Text, Button, View } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import HistoryChart from './HistoryChart'
+
+const countPerDate = (crisisesDateArray) => {
+  const values = [
+    new Date('2023-07-11T08:15:33.968Z'),
+    new Date('2023-07-11T08:15:58.610Z'),
+    new Date('2023-07-10T08:16:24.151Z'),
+    new Date('2023-07-08T08:16:35.877Z'),
+    new Date('2023-07-08T08:16:38.854Z'),
+    new Date('2023-07-08T08:16:39.833'),
+  ]
+
+  const initialValue = {}
+  const grouppedValues = crisisesDateArray.reduce((acc, value) => {
+    if (acc[value.toDateString()] != undefined) acc[value.toDateString()] += 1
+    else {
+      acc[value.toDateString()] = 0
+    }
+    return acc
+  }, initialValue)
+
+  const today = new Date()
+  const countArray = []
+
+  for (let i = 6; i >= 0; i--) {
+    const priorDateString = new Date(new Date().setDate(today.getDate() - i)).toDateString()
+    countArray.push({ date: priorDateString, count: grouppedValues[priorDateString] || 0 })
+  }
+
+  return countArray
+}
 
 const CrisisList = ({ crisises }) => {
   if (!crisises) return <Text>Loading...</Text>
@@ -25,8 +56,6 @@ const storeCrisisesValue = async (value) => {
 
 const History = () => {
   const [history, setHistory] = useState(null)
-  // console.log(history);
-
   const addCrisis = async () => {
     let crisises = await getCrisises()
     if (crisises == null) crisises = []
@@ -54,10 +83,16 @@ const History = () => {
     }
   }
 
-  if (history == null) getCrisises().then((crisises) => setHistory(crisises))
+  if (history == null) {
+    getCrisises().then((crisises) => setHistory(crisises))
+    return <Text>Loading...</Text>
+  }
+
+  const countArray = countPerDate(history.map((dateString) => new Date(dateString)))
 
   return (
     <View>
+      <HistoryChart labels={countArray.map((x) => x['date'])} data={countArray.map((x) => x['count'])} />
       <Text>Crisis History</Text>
       <Button title="Add Crisis" onPress={addCrisis} />
       <Button title="Clear history" onPress={clearCrisises} />
